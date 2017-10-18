b62e092006ea4c3a3ec94fc0e2a05643a5125427
Mishin870 <polsit@yandex.ru>
2017-10-18 15:20:16 +0400

    qwe

--- controllers/CommonController.php
+++ controllers/CommonController.php
@@ -301,22 +301,20 @@ class CommonController extends Core {
 					$i = 0;
 					foreach ($events as $event) {
 						$pid = intval($event->product_id);
-						echo $pid;
 						if (!isset($newStates[$pid])) {
 							$state = $this->products->getState($pid);
-							echo 'state: '.$state;
 							$newStates[$pid] = $state;
 						}
-						var_dump($newStates);
 						$prevState = intval($event->current_state);
-						echo 'prev: '.$prevState;
-						if ($newState != $prevState) {
+						if ($newStates[$pid] != $prevState) {
 							$sendInfo = new stdClass;
 							$sendInfo->id = $i;
 							$i++;
 							$sendInfo->chatid = $event->chatid;
 							$sendInfo->product_id = $event->product_id;
 							$toSend[] = $sendInfo;
+							var_dump($sendInfo);
+							var_dump($toSend);
 						}
 					}
 					//$this->events->deleteAllEvents();
